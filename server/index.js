70f08cbc52ab7be9a938fdc829ee5ef43a3dec90
torsumkhan <torsum.khan@gmail.com>
2022-05-13 03:54:33 +0500

    setup structure

---
+++ server/index.js
@@ -0,0 +1,6 @@
+//Import dependencies. Used import instead of require, since this can be done in new version of node.
+
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
