8973497b25ef98fe81c049dad747ce9de2d5bb5b
torsumkhan <torsum.khan@gmail.com>
2022-05-13 13:51:34 +0500

    connect to mongo db

--- server/index.js
+++ server/index.js
@@ -4,3 +4,28 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+//initialize the app
+
+const app = express();
+
+//Get express app middlewares. limit to 30mb because of image
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//connect to mongo DB - https://www.mongodb.com/atlas/database
+//Save the connection URL to .env later
+const CONNECTION_URL =
+  "mongodb+srv://torsumkhan:Torsum1988@cluster0.sx5di.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
