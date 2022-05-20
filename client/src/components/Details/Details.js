8779125a7ae1491a9ebd25893604618abe8c34c1
torsumkhan <torsum.khan@gmail.com>
2022-05-20 15:30:54 +0500

    create detail page with comments section

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,16 +1,27 @@
 import React, { useEffect } from "react";
+import Header from "../UIComponents/Header";
 import {
-  Paper,
   Typography,
   CircularProgress,
-  Divider,
+  Card,
+  Box,
+  CardContent,
+  Container,
+  CardMedia,
+  TextField,
+  Button,
 } from "@material-ui/core";
+import { useTheme } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
 import useStyles from "./styles";
 import { getCapsule } from "../../actions/capsules";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import { deleteCapsule, likeCapsule } from "../../actions/capsules";
 
 const Details = () => {
+  const theme = useTheme();
   const { post: capsule } = useSelector((state) => {
     console.log(state);
     return state.capsules;
@@ -30,7 +41,86 @@ const Details = () => {
   if (!capsule) {
     return <></>;
   }
-  return <div>{capsule.selectFile}</div>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <div style={{ margin: "2.4rem" }}>
+          <Paper>
+            <Card sx={{ display: "flex", boderRadius: "30px" }}>
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <CardContent sx={{ flex: "1 0 auto" }}>
+                  <Typography component="div" variant="h4">
+                    {capsule.title}
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    Created by: {capsule.name}
+                  </Typography>
+                </CardContent>
+                <CardMedia
+                  component="img"
+                  sx={{ width: 151 }}
+                  image={capsule.selectFile}
+                  alt="Live from space album cover"
+                />
+              </Box>
+
+              <CardContent sx={{ flex: "1 0 auto" }}>
+                <div>
+                  <Button
+                    size="small"
+                    style={{ color: "#9b5de5" }}
+                    onClick={() => dispatch(likeCapsule(capsule.id))}
+                  >
+                    <ThumbUpAltIcon fontSize="small" />
+                    &nbsp; likes &nbsp;
+                    {capsule.like}
+                  </Button>
+                </div>
+                <Typography
+                  component="body2"
+                  variant="h6"
+                  style={{ color: "dark gray" }}
+                >
+                  {capsule.text}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Paper>
+
+          <div style={{ marginTop: "25px" }}>
+            <Typography gutterBottom variant="h6">
+              Write a comment
+            </Typography>
+            <TextField
+              fullWidth
+              rows={4}
+              variant="outlined"
+              label="Comment"
+              multiline
+            />
+            <br />
+            <Button
+              style={{
+                marginTop: "10px",
+                backgroundColor: "#9b5de5",
+                color: "white",
+              }}
+              fullWidth
+              color="primary"
+              variant="contained"
+            >
+              Comment
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default Details;
